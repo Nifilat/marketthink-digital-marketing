9968f870ac8d823afa0eca9515c0df31b909e41d
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-07-23 12:50:14 +0100

    refactor: changed the folder for icons and assets

---
+++ src/app/icons/index.ts
@@ -0,0 +1,9 @@
+export { default as ArrowRightIcon } from './ArrowRightIcon';
+export { SlackIcon, AmazonIcon, LogitechIcon, GoogleIcon, FacebookIcon } from './BrandIcons';
+export { default as CloseIcon } from './CloseIcon';
+export { default as HamburgerIcon } from './HamburgerIcon';
+export { default as HorizontalLine } from './HorizontalLine';
+export { default as Logo } from './Logo';
+export { CubeIcon, PyramidIcon, CircleIcon, TriangleIcon, GreenPyramidIcon } from './ServiceIcons';
+export { default as SquigglyLine } from './SquigglyLine';
+export { default as VerticalDottedLine } from './VerticalDottedLine';
---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import show from './svgs/Show.svg';
+import bgImage from './svgs/bg-image.svg';
+import contactBlock from './svgs/contact-block.svg';
+import dottedLine from './svgs/dottedLine.svg';
+import ServiceImage from './svgs/ServiceImage.svg';
+import facebook from './svgs/facebook.svg';
+import footerLogo from './svgs/footer-logo.svg';
+import hero from './svgs/hero.svg';
+import instagram from './svgs/instagram.svg';
+import left from './svgs/Left.svg';
+import leftEllipse from './svgs/LeftEllipse.svg';
+import right from './svgs/right.svg';
+import rightEllipse from './svgs/RightEllipse.svg';
+import twitter from './svgs/twitter.svg';
+
+export {
+  show,
+  bgImage,
+  contactBlock,
+  dottedLine,
+  ServiceImage,
+  facebook,
+  footerLogo,
+  hero,
+  instagram,
+  left,
+  leftEllipse,
+  right,
+  rightEllipse,
+  twitter,
+};
